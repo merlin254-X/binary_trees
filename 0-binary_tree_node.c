d972474175d46664547f05e9e7ae500ae57854cd
merlin254-X <merlinderick8@gmail.com>
2024-02-27 21:12:26 -0800

    T 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,21 +2,19 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node of the node to create
+ * @parent: pointer to the parent node of the node create
  * @value: Value to put in the new node
  *
- * Return: Pointer to the new node, or NULL on failure
+ * Return: pointer to the new node, return NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	/* Allocate memory for new node */
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Initialize new node */
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
