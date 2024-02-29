4ead1eac3cc2bea6f9c980431559e171b5a00330
merlin254-X <merlinderick8@gmail.com>
2024-02-29 12:16:21 -0800

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,50 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
- * @first: Pointer to the first node
- * @second: Pointer to the second node
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: Pointer to the lowest common ancestor node, or NULL if not found
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									 const binary_tree_t *second)
 {
-	const binary_tree_t *ptr;
-
-	if (first == NULL || second == NULL)
+	if (!first || !second)
 		return (NULL);
 
-	ptr = first;
-	while (ptr != NULL)
-	{
-		if (binary_tree_is_descendant_of(second, ptr))
-			return ((binary_tree_t *)ptr);
-		ptr = ptr->parent;
-	}
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	return (NULL);
-}
+	binary_tree_t *mom = (binary_tree_t *)first->parent,
+				   *pop = (binary_tree_t *)second->parent;
 
-/**
- * binary_tree_is_descendant_of - Checks if a node is descendant of another
- * @node: Pointer to the node to check if it's a descendant
- * @ancestor: Pointer to the ancestor node to check
- *
- * Return: 1 if node is a descendant of ancestor, 0 otherwise
- */
-int binary_tree_is_descendant_of(const binary_tree_t *node,
-								 const binary_tree_t *ancestor)
-{
-	if (node == NULL || ancestor == NULL)
-		return (0);
-
-	while (node != NULL)
-	{
-		if (node == ancestor)
-			return (1);
-		node = node->parent;
-	}
-
-	return (0);
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
