3d69b4371371a9bd9e9a05d29aacb0108aad3857
merlin254-X <merlinderick8@gmail.com>
2024-03-01 12:17:07 -0800

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,23 +5,28 @@
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
  *
- * Return: If no common ancestors return NULL, else return common ancestor.
+ * Return: Pointer to the lowest common ancestor node, or NULL if none found.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+				const binary_tree_t *second)
 {
-	if (!first || !second)
+	if (!first || !second)	/* Check for NULL nodes */
 		return (NULL);
 
-	if (first == second)
+	if (first == second)  /* Check if nodes are the same */
 		return ((binary_tree_t *)first);
 
-	binary_tree_t *mom = (binary_tree_t *)first->parent,
-				   *pop = (binary_tree_t *)second->parent;
+	/* Recursively traverse up the tree from each node */
+	binary_tree_t *p = first->parent;
+	binary_tree_t *q = second->parent;
 
-	if (first == pop || !mom || (!mom->parent && pop))
-		return (binary_trees_ancestor(first, pop));
-	if (mom == second || !pop || (!pop->parent && mom))
-		return (binary_trees_ancestor(mom, second));
-	return (binary_trees_ancestor(mom, pop));
+	while (p && q && p != q)
+	{
+		if (p->depth > q->depth)  /* Move up shorter branch */
+			p = p->parent;
+		else
+			q = q->parent;
+	}
+
+	return (p);  /* Return the common ancestor node */
 }
